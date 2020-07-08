009a9e8d6744460a6bac070debcaf91276deabe7
RosemaryChan <63425905+RosemaryChan@users.noreply.github.com>
2020-07-08 16:07:05 -0700

    Update Main.java

--- Rock Papaer scirror/src/Main.java
+++ Rock Papaer scirror/src/Main.java
@@ -8,42 +8,42 @@ public class    Main
         System.out.println("Choose rock, paper, or scissors. 'r' for rock, 'p' for paper and 's' for scissor");//printing out the line in bracket
         int cchoice = (int) (Math.random() * 3);// Computer would randomly generate three numbers
         if (cchoice == 0) {  //assigning names for each of these possible numbers so that afterwards it prints out names insdead of numbers.
-            cname += "rock";
+            cname += "Rock";
         }
         if (cchoice == 1) {
-            cname += "paper";
+            cname += "Paper";
         }
         if (cchoice == 2) {
-            cname += "scissor";
+            cname += "Scissor";
         }
         char player = scanner.next().charAt(0);//User input, I had to go online for this, trying to make this work was a bit of a struggle
         if (player == 'r' || player == 'p' || player == 's') {//grouping for valid input
             if (player == 'r') {// according to player's choice, giving player's choice a value, and also assigning the whole name to the"name" variable
                 pchoice += 0;
-                pname += "rock";
+                pname += "Rock";
             }
             if (player == 'p') {
                 pchoice += 1;
-                pname += "paper";
+                pname += "Paper";
             }
             if (player == 's') {
                 pchoice += 2;
-                pname += "scissor";
+                pname += "Scissor";
             }
             if (pchoice > cchoice) { // Comparing user & computer's choices
                 if (pchoice == 2 && cchoice == 0) {// the player would win except for when the player chose scissor & computer chose rock
-                    System.out.println("You lost");
+                    System.out.println("You Lose!");
                 } else {
-                    System.out.println("You won");
+                    System.out.println("You Win!");
                 }
-            } else if (cchoice < pchoice) {// the player would lose except for when the player chose rock & computer chose scissor
+            } else if (pchoice < cchoice) {// the player would lose except for when the player chose rock & computer chose scissor
                 if (cchoice == 2 && pchoice == 0) {
-                    System.out.println("You won");
+                    System.out.println("You Win!");
                 } else {
-                    System.out.println("You lost");
+                    System.out.println("You Lost!");
                 }
             } else if (cchoice == pchoice) {
-                System.out.println("It's a tie");
+                System.out.println("Draw!");
             }
             System.out.println("The computer chose:  " + cname + "      The player chose:  " + pname);
         } else {// grouping for every other possible input, which are invalid
